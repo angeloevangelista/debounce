cae340759dfec39fa25ae6e959d9e47483fc2b82
angeloevangelista <angeloevan.ane@gmail.com>
2020-12-01 10:53:30 -0300

    Refactor: Using functions

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-let timeout = null;
-
 const searchUsers = (name) =>
   fetch(
     `http://jsonplaceholder.typicode.com/users?name_like=${name}`,
   ).then((response) => response.json());
 
-function handleKeyUp(event) {
-  clearTimeout(timeout);
+function debounceEvent(fn, wait = 1000, timeout) {
+  return function () {
+    clearTimeout(timeout);
 
-  timeout = setTimeout(async () => {
-    searchUsers(event.target.value).then((users) =>
-      console.log(users.map((u) => u.name)),
-    );
-  }, 1000);
+    timeout = setTimeout(() => fn.apply(this, arguments), wait);
+  };
 }
+
+const handleKeyUp = (event) =>
+  searchUsers(event.target.value).then((users) =>
+    console.log(users.map((u) => u.name)),
+  );
 
 document
   .querySelector('input[name="search"]')
-  .addEventListener('keyup', handleKeyUp);
+  .addEventListener('keyup', debounceEvent(handleKeyUp, 1000));
