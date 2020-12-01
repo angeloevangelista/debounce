9bd7534c3368f83ad80a8578bf9917949ef22f88
angeloevangelista <angeloevan.ane@gmail.com>
2020-12-01 10:17:14 -0300

    Working with users

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-feather ? feather.replace() : null;
+let timeout = null;
+
+const searchUsers = (name) =>
+  fetch(
+    `http://jsonplaceholder.typicode.com/users?name_like=${name}`,
+  ).then((response) => response.json());
+
+function handleKeyUp(event) {
+  clearTimeout(timeout);
+
+  timeout = setTimeout(async () => {
+    searchUsers(event.target.value).then((users) =>
+      console.log(users.map((u) => u.name)),
+    );
+  }, 1000);
+}
+
+document
+  .querySelector('input[name="search"]')
+  .addEventListener('keyup', handleKeyUp);
